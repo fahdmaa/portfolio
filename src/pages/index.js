557e07db4b131942b5aecd96e7bed4a0a0fc23e6
Fahdmaa <f.maatoug.ced@uca.ac.ma>
2024-11-27 19:48:38 +0100

    Fix import path and add propoer Next.js configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Portfolio from 'C:\Users\fahdm\OneDrive\1. Documents\GitHub\portfolio\src\components\Portfolio';
+import Portfolio from 'C:/Users/fahdm/OneDrive/1. Documents/GitHub/portfolio/src/components/Portfolio';
 
 export default function Home() {
   return <Portfolio />;
